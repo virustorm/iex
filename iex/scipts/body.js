a100f2488f8e20a32fe11682a9b75359a4251286
kevin <chengyu0718@hotmail.com>
2019-10-23 13:55:43 -0700

    search engine work with searching symbole or id or name

--- iex/scipts/body.js
+++ iex/scipts/body.js
@@ -14,10 +14,46 @@ function getSymbol(div, data) {
   for (i = 0; i < 101; i++) {
     var ranNum = getRandomNum(0, 8886);
     let divBody = document.createElement("div");
-    let h1 = document.createElement("h1");
-    let text = document.createTextNode(data[ranNum].symbol);
-    h1.appendChild(text);
-    divBody.appendChild(h1);
+    divBody.classList.add("symbol-card");
+    let symbolName = document.createElement("h2");
+    symbolName.classList.add("symbol-text");
+    var text = document.createTextNode(data[ranNum].symbol);
+    symbolName.appendChild(text);
+    let stockName = document.createElement("h3");
+    stockName.classList.add("stock-name");
+    text = document.createTextNode(data[ranNum].name);
+    stockName.appendChild(text);
+    let stockId = document.createElement("h3");
+    stockId.classList.add("stock-id");
+    text = document.createTextNode(`IEX ID: ${data[ranNum].iexId}`);
+    stockId.appendChild(text);
+
+    divBody.appendChild(symbolName);
+    divBody.appendChild(stockName);
+    divBody.appendChild(stockId);
     div.appendChild(divBody);
+  }
+}
+
+function searchFilter() {
+  var input = document.getElementById("search-box");
+  var filter = input.value.toUpperCase();
+  var div = document.getElementsByClassName("symbol-card");
+  for (i = 0; i < div.length; i++) {
+    sText = div[i].getElementsByClassName("symbol-text")[0];
+    sText2 = div[i].getElementsByClassName("stock-name")[0];
+    sText3 = div[i].getElementsByClassName("stock-id")[0];
+    var txtValue = sText.textContent || sText.innerText;
+    var txtValue2 = sText2.textContent || sText.innerText;
+    var txtValue3 = sText3.textContent || sText.innerText;
+    if (
+      txtValue.toUpperCase().indexOf(filter) > -1 ||
+      txtValue2.toUpperCase().indexOf(filter) > -1 ||
+      txtValue3.toUpperCase().indexOf(filter) > -1
+    ) {
+      div[i].style.display = "";
+    } else {
+      div[i].style.display = "none";
+    }
   }
 }
