2c678d292840077ea65c12e906a50e7b3f4756db
kevin <chengyu0718@hotmail.com>
2019-10-23 11:00:54 -0700

    got 100 random symbols

---
+++ iex/scipts/body.js
@@ -0,0 +1,25 @@
+const url = "https://api.iextrading.com/1.0/";
+const dataSymbol = "ref-data/symbols";
+
+axios.get(url + dataSymbol).then(response => test(body, response.data));
+
+function getRandomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+console.log(getRandomNum(0, 8886));
+
+let body = document.getElementById("symbol");
+function test(div, data) {
+  for (i = 0; i < 101; i++) {
+    var ranNum = getRandomNum(0, 8886);
+    let divBody = document.createElement("div");
+    let h1 = document.createElement("h1");
+    let text = document.createTextNode(data[ranNum].symbol);
+    h1.appendChild(text);
+    divBody.appendChild(h1);
+    div.appendChild(divBody);
+  }
+}
